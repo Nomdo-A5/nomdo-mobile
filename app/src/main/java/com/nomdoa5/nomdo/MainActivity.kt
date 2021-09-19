e6184702602cdc2125a53f67f9fa364c071a9e47
Alwan Fauzi <alwanfauzi13@gmail.com>
2021-09-19 19:34:55 +0700

    Initial commit

---
+++ app/src/main/java/com/nomdoa5/nomdo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nomdoa5.nomdo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
