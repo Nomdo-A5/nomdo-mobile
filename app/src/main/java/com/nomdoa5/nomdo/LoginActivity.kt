988d430ef6b4cc9f8213b80a4ba6201e92622a78
Alwan Fauzi <alwanfauzi13@gmail.com>
2021-09-22 11:33:31 +0700

    Redesign Login & Register

--- app/src/main/java/com/nomdoa5/nomdo/LoginActivity.kt
+++ app/src/main/java/com/nomdoa5/nomdo/LoginActivity.kt
@@ -1,11 +1,34 @@
 package com.nomdoa5.nomdo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.nomdoa5.nomdo.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+    private var _binding : ActivityLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignUpIn.setOnClickListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            binding.btnSignUpIn -> {
+                startActivity(Intent(this, RegisterActivity::class.java))
+            }
+        }
     }
 }
--- app/src/main/java/com/nomdoa5/nomdo/RegisterActivity.kt
+++ app/src/main/java/com/nomdoa5/nomdo/RegisterActivity.kt
@@ -1,12 +1,32 @@
 package com.nomdoa5.nomdo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.nomdoa5.nomdo.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
+    private var _binding : ActivityRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        _binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgBack.setOnClickListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            binding.imgBack -> {
+                finish()
+            }
+        }
     }
 }
