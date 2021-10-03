50848cfbc7bed7d91b587ef20a7f733c244fc789
Alwan Fauzi <alwanfauzi13@gmail.com>
2021-10-03 14:42:19 +0700

    Add Detail Task UI

---
+++ app/src/main/java/com/nomdoa5/nomdo/ui/task/DetailTaskFragment.kt
@@ -0,0 +1,23 @@
+package com.nomdoa5.nomdo.ui.task
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nomdoa5.nomdo.R
+
+class DetailTaskFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_task, container, false)
+    }
+}
