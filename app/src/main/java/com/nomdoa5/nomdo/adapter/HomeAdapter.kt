6efb9879542eb85f961afb6f353d751eb8910c17
Mayuzumi05 <alannovianto11@gmail.com>
2021-10-05 21:51:33 +0700

    Add Detail Task Fragment
    closes #15

---
+++ app/src/main/java/com/nomdoa5/nomdo/adapter/HomeAdapter.kt
@@ -0,0 +1,67 @@
+package com.nomdoa5.nomdo.adapter
+
+import android.graphics.Rect
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nomdoa5.nomdo.R
+import com.nomdoa5.nomdo.databinding.ItemHomeBinding
+import com.nomdoa5.nomdo.model.Task
+
+class HomeAdapter : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+    private val mData = ArrayList<Task>()
+    private var onItemClickCallback: OnItemClickCallback? = null
+
+    fun setData(items: ArrayList<Task>) {
+        mData.clear()
+        mData.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Task)
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): HomeViewHolder {
+        val mView = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_home, viewGroup, false)
+
+        return HomeViewHolder(mView)
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        holder.bind(mData[position])
+    }
+
+    override fun getItemCount(): Int = mData.size
+
+    inner class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemHomeBinding.bind(itemView)
+        fun bind(homeItem: Task) {
+            binding.tvTitleHome.text = homeItem.taskName
+
+            itemView.setOnClickListener { onItemClickCallback?.onItemClicked(homeItem) }
+        }
+    }
+
+    class MarginItemDecoration(private val spaceHeight: Int) : RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(
+            outRect: Rect, view: View,
+            parent: RecyclerView, state: RecyclerView.State
+        ) {
+            with(outRect) {
+                if (parent.getChildAdapterPosition(view) == 0) {
+                    top = spaceHeight * 2
+                }
+                left = spaceHeight * 2
+                right = spaceHeight * 2
+                bottom = spaceHeight
+            }
+        }
+    }
+}
--- app/src/main/java/com/nomdoa5/nomdo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nomdoa5/nomdo/ui/home/HomeFragment.kt
@@ -1,44 +1,49 @@
 package com.nomdoa5.nomdo.ui.home
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.nomdoa5.nomdo.R
+import com.nomdoa5.nomdo.adapter.HomeAdapter
 import com.nomdoa5.nomdo.databinding.FragmentHomeBinding
+import com.nomdoa5.nomdo.model.Task
 
 class HomeFragment : Fragment() {
-
-    private lateinit var homeViewModel: HomeViewModel
+    //    private lateinit var myWorkspacesViewModel: MyWorkspacesViewModel
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val homeAdapter = HomeAdapter()
+    private var homes = arrayListOf<Task>()
+    private lateinit var homeName: Array<String>
+    private lateinit var createdAt: Array<String>
+    private lateinit var rvHome: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+//        myWorkspacesViewModel =
+//            ViewModelProvider(this).get(MyWorkspacesViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
+//        myWorkspacesViewModel.text.observe(viewLifecycleOwner, {
+//            textView.text = it
+//        })
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setData()
+        setupRecyclerView()
     }
 
     override fun onDestroyView() {
@@ -46,8 +51,32 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    fun setActionBarTitle(title: String) {
-        val tvTitle = view?.findViewById<TextView>(R.id.tv_title_board)
-        tvTitle!!.text = title
+    fun setData(){
+        homeName = resources.getStringArray(R.array.name)
+        createdAt = resources.getStringArray(R.array.creator)
+
+        for(i in homeName.indices){
+            val home = Task(
+                i,
+                homeName[i],
+                "Owned by " + createdAt[i],
+            )
+            homes.add(home)
+        }
+    }
+
+    fun setupRecyclerView(){
+        rvHome = requireView().findViewById(R.id.rv_homes)
+        rvHome.setHasFixedSize(true)
+        rvHome.addItemDecoration(HomeAdapter.MarginItemDecoration(15))
+        rvHome.layoutManager = LinearLayoutManager(context)
+        homeAdapter.setData(homes)
+        rvHome.adapter = homeAdapter
+
+        homeAdapter.setOnItemClickCallback(object : HomeAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Task) {
+                Snackbar.make(requireView(), "Kamu mengklik #${data.idTask}", Snackbar.LENGTH_SHORT).show()
+            }
+        })
     }
 }
